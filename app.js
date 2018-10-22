0496f22025dc00dafb7937b460d8d87dde346d01
Amani Ben Azzouz <40391719+cs9527@users.noreply.github.com>
2018-10-22 14:05:54 +0200

    redirect urls

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const url = require('url'); 
+const request = require('request');
 
 app.get('/', (req, res) => {
   const source = req.query.source ? req.query.source : 'World';
   res.send(`Hello ${source}`);
 });
 
+
 app.get('/vv(/*)?', (req, res) => {
-   const urlData = req.url.match(/\/vv\/(.*)(\/)?/);
-   res.redirect(url.format({
-       pathname:"/",
-       query: {  "source":  urlData.length > 0 ? urlData[1] : '' }
-   }));
+  const urlData = req.url.match(/\/vv\/(.*)(\/)?/);
+  const souce = urlData.length > 0 ? urlData[1] : '';
+  request(`http://localhost:3000/${souce}`, { json: true }, (err, response, body) => {
+    if (err) { return console.log(err); }
+     res.send(body);
+ })
 });
 
-var server = app.listen(3000, () => {
+var server = app.listen(4000, () => {
 });
